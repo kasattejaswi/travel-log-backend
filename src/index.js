ac52e5f2754e758cdfa1bddeccce073ef5d67ed4
Tejaswi Kasat <kasattejasvi@gmail.com>
2021-12-01 23:46:26 +0530

    Added context root

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ const express = require('express')
 const user = require('./routes/user')
 const posts = require('./routes/post')
 const db = require('./db/connection')
-
+const context = '/api/v1'
 const app = express()
 app.use(express.json())
-app.use(user)
-app.use(posts)
+app.use(context, user)
+app.use(context, posts)
 
 app.listen(3000, () => {
     console.log("Application is running on port 3000")
